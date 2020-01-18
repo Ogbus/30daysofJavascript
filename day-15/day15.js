8505ba5ba8708c29a4d62757554f7b15be20dd51
Felix Ogbuluijah <felixogbuluijah@yahoo.com>
2020-01-18 21:55:19 +0100

    Creating more classes

--- day-15/day15.js
+++ day-15/day15.js
@@ -13,3 +13,21 @@ class Person {
   console.log(person1)
   console.log(person2)
   console.log(person3)
+
+// Create an Animal class. The class will have name, age, color, legs properties and create different methods
+class Animal {
+    constructor(name, age, color, legs) {
+        console.log(this);
+        this.name = name
+        this.age = age
+        this.color = color
+        this.legs = legs
+    }
+    getLegs() {
+        return this.legs;
+    }
+}
+
+let animal1 = new Animal('dog', 34, 'purple', 4)
+console.log(Animal);
+console.log(animal1.getLegs());
